86cfbeefe250900864b7671112f5d7246e87f909
Ashish-kumar-Giri <ashishgiri1948@gmail.com>
2025-03-28 13:58:57 +0530

    First Commit

---
+++ test.py
@@ -0,0 +1,11 @@
+# Program to add two numbers
+
+# Get input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is: {sum}")
